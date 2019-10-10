d9dff61c63a8ba627ddc4903fa4d1bcfdd355862
lynneeb <bea17025@byui.edu>
2019-10-10 16:06:00 -0700

    changed date format

--- lesson4/js/currentDate.js
+++ lesson4/js/currentDate.js
@@ -28,5 +28,5 @@ const d = new Date();
 const dayName = daynames[d.getDay()];
 const monthName = months[d.getMonth()];
 const year = d.getFullYear();
-const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+const fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
 document.getElementById("currentdate").textContent = fulldate;
