9e471dbf62f003c6b111a4a4eb935e9c3a365e8d
lynneeb <bea17025@byui.edu>
2019-10-10 13:00:56 -0700

    fixed menu collapsed

--- lesson4/js/toggleMenu.js
+++ lesson4/js/toggleMenu.js
@@ -1,4 +1,5 @@
 
 function toggleMenu() {
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+	document.getElementsByClassName("navigation")
+	[0].classList.toggle("responsive");
 }
