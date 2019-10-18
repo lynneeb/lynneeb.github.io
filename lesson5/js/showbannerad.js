4ab5d40db2043df7da458cee01b4b2e0ecd9865a
lynneeb <bea17025@byui.edu>
2019-10-18 11:02:52 -0700

    showbannerad

---
+++ lesson5/js/showbannerad.js
@@ -0,0 +1,15 @@
+// function showbannerad() {
+// 	var d = new Date();
+// 	var day = d.getDay();
+// 		if (day==5) { 
+// 			$("#bannerad").css("display","block");
+// 		}
+//   }
+
+
+  function showbannerad() {
+    	var x = document.getElementById('bannerad');
+    	if (d.getDay() == 5) {
+    		x.style.display = 'block';
+    	}
+    }
--- lesson5/js/sources.js
+++ lesson5/js/sources.js
@@ -49,9 +49,59 @@ document.getElementById("currentdate").textContent = fulldate;
 // 	}
 // }
 
+// function togglebannerad() {
+// 	var x = document.getElementById('bannerad');
+// 	if (d.getDay() == [5]) {
+// 		x.style.display = 'none';
+// 	}
+// }
+
+// function togglebannerad() {
+// 	var x = document.getElementById('bannerad').innerHTML;
+// 	if (d.getDay() == [5]) {
+// 		x.style.display = 'none';
+// 	}
+// }
+
+
+/* <script type="text/javascript"></script>
+var day=d.getDay();
 function togglebannerad() {
-	var x = document.getElementById('bannerad');
-	if (d.getDay() !== 5) {
-		x.style.visibility = 'hidden';
-	}
-}
+        if (day==5) { 
+            $("#bannerad").css("display","block");
+
+} */
+
+// function showbannerad() {
+// 	var d = new Date();
+// 	var day = d.getDay();
+// 		if (day==5) { 
+// 			$("#bannerad").css("display","block");
+// 		}
+//   }
+
+
+/* <script type="text/javascript">
+var date=new Date();
+var year=date.getFullYear();
+var month=date.getMonth();
+var day=date.getDate(); // fixed
+
+function SetDivContent() { */
+
+    // if (year==2014 && month==00) { 
+        // if (day>=3 && day<29) { 
+            // $("#first").css("display","block");
+        // }
+//         else if (day==11 || day==12) { 
+//             $("#second").css("display","block");
+//         }
+//         else if (day>12) { 
+//             $("#third").css("display","block");
+//         }
+//     // }
+//     else if (year==2014 && month>=0) { 
+//             $("#third").css("display","block");
+//         }
+//     }
+// </script>
