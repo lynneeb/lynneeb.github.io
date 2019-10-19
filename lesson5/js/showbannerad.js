fb48c8aa5d5527d36ccb7c0c982081006f67f768
lynneeb <bea17025@byui.edu>
2019-10-18 17:34:54 -0700

    showbannerad

--- lesson5/js/showbannerad.js
+++ lesson5/js/showbannerad.js
@@ -7,9 +7,31 @@
 //   }
 
 
-  function showbannerad() {
-    	var x = document.getElementById('bannerad');
-    	if (d.getDay() == 5) {
-    		x.style.display = 'block';
-    	}
+//   function showbannerad() {
+//     	var x = document.getElementById('bannerad');
+//     	if (d.getDay() == 5) {
+//     		x.style.display = 'block';
+//     	}
+//     }
+
+
+    // function showbannerad() {
+    //     if (d.getDay() === 5) {
+    //     document.getElementsByClassName("bannerad")[0].classList.toggle("showbannerad");
+    //     }
+    // }
+
+
+    // function showbannerad() {
+    //     var d = new Date();
+    //     var n = d.getDay()
+    //     document.getElementById("bannerad").innerHTML = n;
+    //     if (n == 5) {
+    //         n.style.display = 'block';
+    //     }
+    // }
+
+
+    if (d.getDay() === 5) {
+        document.getElementById("bannerad").hidden = false;
     }
