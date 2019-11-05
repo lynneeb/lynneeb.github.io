3663b59dddcc639c5a09dc7fc575378723f715fb
lynneeb <bea17025@byui.edu>
2019-11-05 12:15:39 -0700

    tablebuild commit

---
+++ lesson8/js/date.js
@@ -0,0 +1,8 @@
+// The footer must display the current date using JavaScript 
+// in this format 11.5.2019
+
+
+const d = new Date(); 
+const month = parseInt(d.getMonth() + 1);
+const outdate = month + "." + d.getDate() + "." + d.getFullYear();
+document.getElementById("currentdate").textContent = outdate;
