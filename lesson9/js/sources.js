1d88453efc89dea6c54d16ce32e1f8812d256014
lynneeb <bea17025@byui.edu>
2019-11-16 15:35:36 -0700

    townsites initial commit

---
+++ lesson9/js/sources.js
@@ -0,0 +1,66 @@
+// **********  turn on the hamburger menu in small viewport  **********
+function togglemenu() {
+	document.getElementsByClassName("navigation")
+	[0].classList.toggle("responsive");
+}
+
+// The footer must display the current date using JavaScript 
+// in this format Wednesday, 24 July 2020
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
+document.getElementById("currentdate").textContent = fulldate;
+
+
+// **********  Only make the bannerad visible on Friday  ****************
+
+// function showbannerad() {
+//     if (d.getDay() === 5) {
+//         document.getElementById("bannerad").style.display = "block";
+//     }
+// }
+
+//code for storm severtiy rating
+// function adjustRating(rating) {
+//     document.getElementById("ratingvalue").innerHTML = rating;
+// }
+
+
+// get fonts
+function getfonts() {
+	WebFont.load({google: {families: ["Baloo Chettan","Raleway"]}});
+}
+
+
+// call the functions
+function start() {
+    // showbannerad();
+	// doInputOutput();
+	getfonts();
+}
