026ad8a58f84992e6ca9127e9214b059db2224a7
lynneeb <bea17025@byui.edu>
2019-12-11 20:12:44 -0700

    temple website

---
+++ lesson13/js/events-sp.js
@@ -0,0 +1,18 @@
+  fetch('https://byui-cit230.github.io/weather/data/towndata.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    // console.table(jsonObject);
+    const towns = jsonObject['towns'];
+    var town = towns.find(town => town.name === "Soda Springs");
+    var list = document.createElement('ul');
+    for (var event of town.events) {
+    let item = document.createElement('li');
+    item.textContent = event;
+    list.appendChild(item);
+    }
+
+    document.querySelector('div.eventlist').appendChild(list);
+
+  });
